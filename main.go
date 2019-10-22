cc99fa9e98883e4fa4e5d5b42197378db502409f
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2019-10-23 00:38:28 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	// initialize game
+
+	// load resources
+
+	// game loop
+
+	for{
+		// update screen
+
+		// process input
+
+		// process movement
+
+		// check game over?
+
+		//Temp:break infinte loop
+		fmt.Println("Hello, pac Go!")
+		break
+
+		// repeat
+	}
+}
